a696440c0db6733ee4d41b392366acfae582fe7a
rishabhplay450-cloud <rishabhplay450@gmail.com>
2025-11-02 11:08:52 +0530

    first commit

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a = 8
+b = 4
+print("Addition of",a,"and",b,"is:", a+b)
+print("Subtraction of",a,"and",b,"is:", a-b)
+print("Multiplication of",a,"and",b,"is:", a*b)
+print("Division of",a,"and",b,"is:", a/b)
+print("Module of",a,"and",b,"is:", a%b)
+print("Exponential of",a,"and",b,"is:", a**b)
+print("Floor Division of",a,"and",b,"is:", a//b)
